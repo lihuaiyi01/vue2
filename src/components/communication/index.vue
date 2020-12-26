ede121953eda224dbc882369617b47e1edb94e94
lihuaiyi01 <1195841709@qq.com>
2020-12-26 17:00:04 +0800

    111

---
+++ src/components/communication/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>组件通信</h2>
+        <!-- props, 自定义事件 -->
+        <Child1 msg="some msg from parent" @some-event="onSomeEvent"></Child1>
+        <!-- 事件总线 -->
+        <Child2></Child2>
+    </div>
+</template>
+
+<script>
+import Child1 from '../communication/Child1'
+import Child2 from '../communication/Child2'
+
+export default {
+    components: {
+        Child1, Child2
+    },
+    methods: {
+        onSomeEvent(msg) {
+            console.log('Communition:', msg)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
